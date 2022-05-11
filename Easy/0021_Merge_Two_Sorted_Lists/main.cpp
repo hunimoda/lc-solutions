dfe05c2169cb9d11d82fcc0a314641e38e59baf7
hunimoda <hunimoda@gmail.com>
2022-05-11 20:31:39 +0900

    Easy: problem 21. merge two sorted lists ver. 1

---
+++ Easy/0021_Merge_Two_Sorted_Lists/main.cpp
@@ -0,0 +1,46 @@
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+class Solution {
+
+public:
+    ListNode *merge_two_lists(ListNode *list1, ListNode * list2) {
+
+        // If the list is empty, return the other list
+        if (!list1) return list2;
+        if (!list2) return list1;
+
+        // Determine which list comes first
+        ListNode *front {
+            (list1->val < list2->val) ? list1 : list2
+        };
+        ListNode *back {
+            (list1->val < list2->val) ? list2 : list1
+        };
+        ListNode *merged { front };
+
+        // Move front pointer to the cut point
+        while (front->next != nullptr &&
+                front->next->val < back->val) {
+            front = front->next;
+        }
+        ListNode *next_front { front->next };
+
+        // Partially sort by moving pointers
+        front->next = back;
+        front = next_front;
+
+        // Recursion
+        back->next = merge_two_lists(front, back->next);
+        
+        return merged;
+
+    }
+
+};
