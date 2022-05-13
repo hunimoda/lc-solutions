cb003dd79c723d816b58944ad83385fa0fb98e7a
hunimoda <hunimoda@gmail.com>
2022-05-13 16:39:14 +0900

    Easy: problem 67. add binary ver. 2 amortizing

--- Easy/0067_Add_Binary/main.cpp
+++ Easy/0067_Add_Binary/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include <string>
 using std::string;
 
@@ -8,26 +9,25 @@ class Solution {
 public:
     string add_binary(string a, string b) {
 
-        if (a.length() < b.length()) return add_binary(b, a);
+        string sum { "" };
+        int carry { 0 };
 
-        const int diff { (int) (a.length() - b.length()) };
-        int carry_in { 0 };
-        string sum_str { "" };
+        int idx_a { (int) a.length() - 1 };
+        int idx_b { (int) b.length() - 1 };
 
-        for (int i { (int) a.length() - 1 }; i >= 0; i--) {
-            int digit_a { a.at(i) - '0' };
-            int digit_b {
-                (i >= diff) ? b.at(i - diff) - '0' : 0
-            };
+        while (idx_a >= 0 || idx_b >= 0) {
+            if (idx_a >= 0) carry += a[idx_a--] - '0';
+            if (idx_b >= 0) carry += b[idx_b--] - '0';
 
-            int sum = digit_a + digit_b + carry_in;
-            sum_str = std::to_string(sum % 2) + sum_str;
-            carry_in = sum / 2;
+            sum += (char) (carry % 2 + '0');
+            carry /= 2;
         }
+        
+        if (carry) sum += '1';
 
-        sum_str = (carry_in == 1 ? "1" : "") + sum_str;
+        std::reverse(sum.begin(), sum.end());
 
-        return sum_str;
+        return sum;
 
     }
 
