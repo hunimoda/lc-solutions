0fb1cfa6060c76db6265c45413b9182246dc7e13
hunimoda <hunimoda@gmail.com>
2022-05-14 15:25:29 +0900

    Easy: problem 88. merge sorted array ver.2

--- Easy/0088_Merge_Sorted_Array/main.cpp
+++ Easy/0088_Merge_Sorted_Array/main.cpp
@@ -5,19 +5,13 @@ using std::vector;
 class Solution {
  public:
   void Merge(vector<int> &nums1, int m, vector<int> &nums2, int n) {
-    vector<int> result{};
-    int i{}, j{};
-    while (i < m && j < n) {
-      if (nums1[i] < nums2[j]) {
-        result.push_back(nums1[i++]);
-      } else {
-        result.push_back(nums2[j++]);
-      }
+    int i{m - 1}, j{n - 1};
+    int k{m + n - 1};
+    while (i >= 0 && j >= 0) {
+      nums1[k--] =
+          nums1[i] > nums2[j]
+          ? nums1[i--]
+          : nums2[j--];
     }
-    if (i < m)
-      result.insert(result.end(), nums1.begin() + i, nums1.begin() + m);
-    if (j < n)
-      result.insert(result.end(), nums2.begin() + j, nums2.end());
-    nums1 = std::move(result);
   }
 };
