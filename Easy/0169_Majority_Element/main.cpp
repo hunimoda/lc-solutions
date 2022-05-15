2c8c4a0e34d8936d4ded3ff624c21780f60b8a99
hunimoda <hunimoda@gmail.com>
2022-05-15 09:56:37 +0900

    Easy: problem 169. majority element - Moore's majority vote algorithm

--- Easy/0169_Majority_Element/main.cpp
+++ Easy/0169_Majority_Element/main.cpp
@@ -5,6 +5,10 @@ using std::vector;
 class Solution {
  public:
   int MajorityElement(vector<int> &nums) {
-    //
+    int cand{}, count{0};
+    for (int num: nums) {
+      if (count == 0) cand = num;
+      count += (num == cand) ? 1 : -1;
+    }
   }
 };
