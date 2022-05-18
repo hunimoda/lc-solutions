ca9e65200087c6889452f8a48f5b456d17cee01f
hunimoda <hunimoda@gmail.com>
2022-05-18 12:51:11 +0900

    Easy: problem 231. power of two - check if log2 is integer

--- Easy/0231_Power_of_Two/main.cpp
+++ Easy/0231_Power_of_Two/main.cpp
@@ -1,8 +1,15 @@
+#include <iostream>
+#include <cmath>
+
 class Solution {
+ private:
+  bool IsInteger(double x) {
+    return x == std::floor(x);
+  }
+
  public:
   bool IsPowerOfTwo(int n) {
     if (n < 1) return false;
-    if (n == 1) return true;
-    return (n % 2 == 0) && IsPowerOfTwo(n / 2);
+    return IsInteger(std::log2(n));
   }
 };
