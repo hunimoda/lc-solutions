05e9b373e2fc1bdb90d14d24b72e178658479dbb
hunimoda <hunimoda@gmail.com>
2022-05-18 12:34:57 +0900

    Easy: problem 231. power of two - loop

---
+++ Easy/0231_Power_of_Two/main.cpp
@@ -0,0 +1,11 @@
+class Solution {
+ public:
+  bool IsPowerOfTwo(int n) {
+    if (n < 1) return false;
+    while (n > 1) {
+      if (n % 2 != 0) return false;
+      n /= 2;
+    }
+    return true;
+  }
+};
