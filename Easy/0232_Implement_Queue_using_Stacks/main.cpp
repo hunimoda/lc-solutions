7e481fb31935df94da1442a9164158a211456a90
hunimoda <hunimoda@gmail.com>
2022-05-18 15:37:49 +0900

    Easy: problem 232. implement queue using stacks - amortized trial

--- Easy/0232_Implement_Queue_using_Stacks/main.cpp
+++ Easy/0232_Implement_Queue_using_Stacks/main.cpp
@@ -1,61 +1,55 @@
 #include <iostream>
 #include <stack>
+#include <chrono>
+
+using namespace std::chrono;
 
 class MyQueue {
  private:
-  std::stack<int> stack;
-  std::stack<int> helper;
+  std::stack<int> up;
+  std::stack<int> down;
 
  public:
   MyQueue() {}
 
   void Push(int x) {
-    const size_t kStackSize = stack.size();
-
-    // Pop all elements: stack -> helper
-    for (size_t i = 0; i < kStackSize; i++) {
-      helper.push(stack.top());
-      stack.pop();
+    if (up.empty()) {
+      const size_t down_size = down.size();
+      for (size_t i = 0; i < down_size; i++) {
+        up.push(down.top());
+        down.pop();
+      }
     }
-
-    // Push x into stack
-    stack.push(x);
-
-    // Push all elements back: helper -> stack
-    for (size_t i = 0; i < kStackSize; i++) {
-      stack.push(helper.top());
-      helper.pop();
-    }
+    up.push(x);
   }
 
   int Pop() {
-    int pop = Peek();
-    stack.pop();
-    return pop;
+    int temp = Peek();
+    down.pop();
+    return temp;
   }
 
-  int Peek() { return stack.top(); }
-  bool Empty() { return stack.empty(); }
+  int Peek() {
+    const size_t up_size = up.size();
+    for (size_t i = 0; i < up_size; i++) {
+      down.push(up.top());
+      up.pop();
+    }
+    return down.top();
+  }
+
+  bool Empty() {
+    return up.empty() && down.empty();
+  }
 };
 
 int main() {
   MyQueue queue;
-  std::cout << std::boolalpha;
-
-  queue.Push(1);
-  std::cout << queue.Peek() << std::endl;
-  queue.Push(2);
-  std::cout << queue.Peek() << std::endl;
-  queue.Push(3);
-  std::cout << queue.Peek() << std::endl;
-
-  std::cout << "------------------------------" << std::endl;
-  std::cout << queue.Pop() << std::endl;
-  std::cout << queue.Peek() << std::endl;
-  std::cout << queue.Pop() << std::endl;
-  std::cout << queue.Peek() << std::endl;
-  std::cout << queue.Pop() << std::endl;
-  std::cout << queue.Peek() << std::endl;
-
+  auto start = high_resolution_clock::now();
+  for (int i = 0; i < 100'000; i++)
+    queue.Push(i);
+  auto stop = high_resolution_clock::now();
+  auto duration = duration_cast<microseconds>(stop - start);
+  std::cout << duration.count() << "[us]" << std::endl;
   return 0;
 }
