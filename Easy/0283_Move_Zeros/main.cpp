fe3ce3cf1c257d7b8659391132ffbe9a4e2d4f59
hunimoda <hunimoda@gmail.com>
2022-05-19 12:43:51 +0900

    Easy: problem 283. move zeros - better solution using swap

--- Easy/0283_Move_Zeros/main.cpp
+++ Easy/0283_Move_Zeros/main.cpp
@@ -5,13 +5,7 @@ using std::vector;
 class Solution {
  public:
   void MoveZeros(vector<int> &nums) {
-    size_t left = 0, right = 0;
-    while (right < nums.size()) {
-      if (nums[right] != 0)
-        nums[left++] = nums[right];
-      right++;
-    }
-    while (left < nums.size())
-      nums[left++] = 0;
+    for (size_t l = 0, r = 0; r < nums.size(); r++)
+      if (nums[r]) std::swap(nums[l++], nums[r]);
   }
 };
