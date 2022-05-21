dbecce98c025a708bf5387d741d7fb1043707288
Dae Hoon Kim <hunimoda@gmail.com>
2022-05-21 22:23:54 +0900

    Easy: problem 326. power of three - iteration

--- Easy/0326_Power_of_Three/main.cpp
+++ Easy/0326_Power_of_Three/main.cpp
@@ -2,7 +2,10 @@ class Solution {
  public:
   bool IsPowerOfThree(int n) {
     if (n <= 0) return false;
-    if (n == 1) return true;
-    return !(n % 3) && IsPowerOfThree(n / 3);
+    while (n > 1) {
+      if (n % 3) return false;
+      n /= 3;
+    }
+    return true;
   }
 };
