1d0cfe1917e26e02a49cc2308f3dddab426a9f6c
Dae Hoon Kim <hunimoda@gmail.com>
2022-05-24 01:35:46 +0900

    Easy: problem 383. ransom note - integer array

--- Easy/0300-0399/0383_Ransom_Note/main.cpp
+++ Easy/0300-0399/0383_Ransom_Note/main.cpp
@@ -5,14 +5,23 @@ using std::string;
 
 class Solution {
  public:
+  // // Unordered map version
+  // bool CanConstruct(string ransom_note, string magazine) {
+  //   std::unordered_map<char, int> counter;
+  //   for (const char &c : magazine)
+  //     counter.count(c) ? counter[c]++ : counter[c] = 1;
+  //   for (const char &c : ransom_note) {
+  //     if (!counter.count(c) || counter[c] <= 0) return false;
+  //     counter[c]--;
+  //   }
+  //   return true;
+  // }
   bool CanConstruct(string ransom_note, string magazine) {
-    std::unordered_map<char, int> counter;
+    int counter[26] = { 0 };
     for (const char &c : magazine)
-      counter.count(c) ? counter[c]++ : counter[c] = 1;
-    for (const char &c : ransom_note) {
-      if (!counter.count(c) || counter[c] <= 0) return false;
-      counter[c]--;
-    }
+      counter[c - 'a']++;
+    for (const char &c : ransom_note)
+      if (counter[c - 'a']-- <= 0) return false;
     return true;
   }
 };
