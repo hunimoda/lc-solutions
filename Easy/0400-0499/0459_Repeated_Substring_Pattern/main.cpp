f97851307ca5508afcbb1c544337c62629018bc0
Dae Hoon Kim <hunimoda@gmail.com>
2022-05-27 22:09:30 +0900

    Easy: problem 459. repeated substring pattern - implement KPM algorithm

--- Easy/0400-0499/0459_Repeated_Substring_Pattern/main.cpp
+++ Easy/0400-0499/0459_Repeated_Substring_Pattern/main.cpp
@@ -1,6 +1,9 @@
+#include <iostream>
 #include <string>
+#include <vector>
 
 using std::string;
+using std::vector;
 
 class Solution {
  public:
@@ -17,4 +20,61 @@ class Solution {
     }
     return false;
   }
+
+  int FindSubstring(string haystack, string needle) {
+    vector<int> lps(FillLpsArray(needle));
+    size_t i = 0, j = 0;
+    while (i < haystack.size() && j < needle.size()) {
+      if (haystack[i] == needle[j]) i++, j++;
+      else if (j == 0) i++;
+      else j = lps[j - 1];
+    }
+    return j == needle.size() ? i - j : -1;
+  }
+
+ private:
+  vector<int> FillLpsArray(string s) {
+    vector<int> lps{0};
+    size_t i = 1, len = 0;
+    while (i < s.size()) {
+      if (s[i] == s[len]) lps[i++] = ++len;
+      else if (len == 0) lps[i++] = 0;
+      else len = lps[len - 1];
+    }
+    return lps;
+  }
 };
+
+void TestSubstring(string haystack, string needle) {
+  Solution solution;
+  const int index = solution.FindSubstring(haystack, needle);
+  std::cout << "\n=========================================================="
+            << std::endl;
+
+  if (index == -1) {
+    std::cout << "Pattern '" << needle << "' not found in:" << std::endl;
+    std::cout << haystack << std::endl;
+  } else {
+    std::cout << "Pattern '" << needle << "' found!" << std::endl;
+    std::cout << "----------------------------------------------------------" 
+              << std::endl;
+    std::cout << haystack << std::endl;
+    for (int i = 0; i < index; i++) std::cout << ' ';
+    std::cout << needle << std::endl;
+  }
+
+  std::cout << "=========================================================="
+            << std::endl;
+}
+
+int main() {
+  Solution solution;
+  TestSubstring("Hello World", "llo");
+  TestSubstring("abcdef", "def");
+  TestSubstring("computer", "mpute8");
+  TestSubstring("stringmatchingmat", "ingmat");
+  TestSubstring("videobox", "videobox");
+  TestSubstring("videobox", "vided");
+  TestSubstring("videobox", "eob");
+  return 0;
+}
