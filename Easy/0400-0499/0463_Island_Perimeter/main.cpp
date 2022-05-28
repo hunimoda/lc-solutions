a171c0604f94d493fcff5636163e89f75c3e84f4
Dae Hoon Kim <hunimoda@gmail.com>
2022-05-28 13:14:55 +0900

    Easy: problem 463. island perimeter - less variables

--- Easy/0400-0499/0463_Island_Perimeter/main.cpp
+++ Easy/0400-0499/0463_Island_Perimeter/main.cpp
@@ -11,10 +11,9 @@ class Solution {
     for (int i = 0; i < i_max; i++) {
       for (int j = 0; j < j_max; j++) {
         if (!grid[i][j]) continue;
-        int more_lands = 0;
-        if (i < i_max - 1 && grid[i + 1][j]) grid[i + 1][j]++, more_lands++;
-        if (j < j_max - 1 && grid[i][j + 1]) grid[i][j + 1]++, more_lands++;
-        perimeter += 5 - grid[i][j] - more_lands;
+        if (i < i_max - 1 && grid[i + 1][j]) grid[i + 1][j]++, perimeter--;
+        if (j < j_max - 1 && grid[i][j + 1]) grid[i][j + 1]++, perimeter--;
+        perimeter += 5 - grid[i][j];
       }
     }
     return perimeter;
