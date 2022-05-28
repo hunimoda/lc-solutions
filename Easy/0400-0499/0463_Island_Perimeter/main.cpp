c57845d5547e4bcdb3f5bee0fa38aa86b8906346
Dae Hoon Kim <hunimoda@gmail.com>
2022-05-28 13:08:03 +0900

    Easy: problem 463. island perimeter - more optimized brute force

--- Easy/0400-0499/0463_Island_Perimeter/main.cpp
+++ Easy/0400-0499/0463_Island_Perimeter/main.cpp
@@ -6,22 +6,17 @@ class Solution {
  public:
   int IslandPerimeter(vector<vector<int>> &grid) {
     int perimeter = 0;
-    for (int i = 0; i < grid.size(); i++) {
-      for (int j = 0; j < grid[i].size(); j++) {
-        if (!IsIsland(grid, i, j)) continue;
-        if (!IsIsland(grid, i - 1, j)) perimeter++;
-        if (!IsIsland(grid, i + 1, j)) perimeter++;
-        if (!IsIsland(grid, i, j - 1)) perimeter++;
-        if (!IsIsland(grid, i, j + 1)) perimeter++;
+    const int i_max = grid.size();
+    const int j_max = grid[0].size();
+    for (int i = 0; i < i_max; i++) {
+      for (int j = 0; j < j_max; j++) {
+        if (!grid[i][j]) continue;
+        int more_lands = 0;
+        if (i < i_max - 1 && grid[i + 1][j]) grid[i + 1][j]++, more_lands++;
+        if (j < j_max - 1 && grid[i][j + 1]) grid[i][j + 1]++, more_lands++;
+        perimeter += 5 - grid[i][j] - more_lands;
       }
     }
     return perimeter;
-  }
-
- private:
-  bool IsIsland(vector<vector<int>> &grid, int i, int j) {
-    return (0 <= i && i < grid.size()) &&
-           (0 <= j && j < grid[i].size()) &&
-           (grid[i][j] == 1);
   }
 };
