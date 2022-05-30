246b81e1d0c0cabba7b5aa7ad1cfcf86b64fe35f
Dae Hoon Kim <hunimoda@gmail.com>
2022-05-31 01:25:01 +0900

    Easy: problem 507. perfect number - O(sqrt(n))

--- Easy/0500-0599/0507_Perfect_Number/main.cpp
+++ Easy/0500-0599/0507_Perfect_Number/main.cpp
@@ -1,9 +1,11 @@
+#include <cmath>
+
 class Solution {
  public:
   bool CheckPerfectNumber(int num) {
-    int sum = 0;
-    for (int div = 1; div <= num / 2; div++)
-      if (num % div == 0) sum += div;
-    return sum == num;
+    int sum = 0, limit = static_cast<int>(std::sqrt(num));
+    for (int div = 1; div <= limit; div++)
+      if (num % div == 0) sum += div + num / div;
+    return sum == 2 * num && num != 1;
   }
 };
