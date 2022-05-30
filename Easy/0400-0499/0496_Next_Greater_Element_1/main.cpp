005a784fdd5f67c4bc3e85d7e50706b70f561b17
Dae Hoon Kim <hunimoda@gmail.com>
2022-05-30 18:24:53 +0900

    Easy: problem 496. next greater element 1 - stack + unordered_map

--- Easy/0400-0499/0496_Next_Greater_Element_1/main.cpp
+++ Easy/0400-0499/0496_Next_Greater_Element_1/main.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <stack>
 #include <unordered_map>
 
 using std::vector;
@@ -6,17 +7,19 @@ using std::vector;
 class Solution {
  public:
   vector<int> NextGreaterElement(vector<int> &nums1, vector<int> &nums2) {
-    std::unordered_map<int, int> map = { {nums2.back(), -1} };
-    for (int i = nums2.size() - 2; i >= 0; i--) {
-      int greater_num = nums2[i + 1];
-      while (greater_num < nums2[i] && greater_num != -1)
-        greater_num = map[greater_num];
-      map[nums2[i]] = greater_num;
+    std::unordered_map<int, int> umap;
+    std::stack<int> stack;
+    for (const int &num : nums2) {
+      while (!stack.empty() && stack.top() < num)
+        umap[stack.top()] = num, stack.pop();
+      stack.push(num);
     }
+    while (!stack.empty())
+      umap[stack.top()] = -1, stack.pop();
 
     vector<int> next_greater_element;
     for (const int &num : nums1)
-      next_greater_element.push_back(map[num]);
+      next_greater_element.push_back(umap[num]);
     return next_greater_element;
   }
 };
