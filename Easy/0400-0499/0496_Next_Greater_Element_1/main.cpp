e91edcc357145819739e4e489338d7a6455b0977
Dae Hoon Kim <hunimoda@gmail.com>
2022-05-30 18:07:02 +0900

    Easy: problem 496. next greater element - improved version

--- Easy/0400-0499/0496_Next_Greater_Element_1/main.cpp
+++ Easy/0400-0499/0496_Next_Greater_Element_1/main.cpp
@@ -1,26 +1,22 @@
 #include <vector>
+#include <unordered_map>
 
 using std::vector;
 
 class Solution {
  public:
   vector<int> NextGreaterElement(vector<int> &nums1, vector<int> &nums2) {
+    std::unordered_map<int, int> map = { {nums2.back(), -1} };
+    for (int i = nums2.size() - 2; i >= 0; i--) {
+      int greater_num = nums2[i + 1];
+      while (greater_num < nums2[i] && greater_num != -1)
+        greater_num = map[greater_num];
+      map[nums2[i]] = greater_num;
+    }
+
     vector<int> next_greater_element;
     for (const int &num : nums1)
-      next_greater_element.push_back(GetNextGreaterElement(nums2, num));
+      next_greater_element.push_back(map[num]);
     return next_greater_element;
-  }
-
- private:
-  int GetNextGreaterElement(const vector<int> &nums, const int target_num) {
-    // Find target_num's index
-    int target_idx;
-    for (target_idx = 0; target_idx < nums.size(); target_idx++)
-      if (nums[target_idx] == target_num) break;
-    
-    // Find next greater element
-    for (size_t i = target_idx + 1; i < nums.size(); i++)
-      if (nums[i] > target_num) return nums[i];
-    return -1;
   }
 };
