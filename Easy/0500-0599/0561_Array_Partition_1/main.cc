be43bc9dc7c41bf9af6afc45ae7fb8eb2ac2180e
Dae Hoon Kim <hunimoda@gmail.com>
2022-05-31 21:52:13 +0900

    Easy: problem 561. array partition 1 - base 256 radix sort

--- Easy/0500-0599/0561_Array_Partition_1/main.cc
+++ Easy/0500-0599/0561_Array_Partition_1/main.cc
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <vector>
 #include <algorithm>
 
@@ -6,10 +7,52 @@ using std::vector;
 class Solution {
  public:
   int ArrayPairSum(vector<int> &nums) {
-    std::sort(nums.begin(), nums.end(), std::greater<>());
-    int sum = 0, nums_size = nums.size();
-    for (size_t i = 1; i < nums_size; i += 2)
+    RadixSort(nums);
+    int sum = 0;
+    for (int i = nums.size() - 2; i >= 0; i -= 2)
       sum += nums[i];
     return sum;
   }
+
+ private:
+  int GetNthDigit(int num, int n) {
+    return (num >> (n << 3)) & 255;
+  }
+
+  void NegateSignBit(vector<int> &nums) {
+    for (int &num : nums)
+      num += 1 << 31;
+  }
+
+ public:
+  void RadixSort(vector<int> &nums) {
+    NegateSignBit(nums);
+    for (int n = 0; n < 4; n++) {
+      vector<vector<int>> buckets(256);
+      for (const int &num : nums)
+        buckets[GetNthDigit(num, n)].push_back(num);
+      nums.clear();
+      for (const auto &bucket : buckets)
+        nums.insert(nums.end(), std::make_move_iterator(bucket.begin()),
+                    std::make_move_iterator(bucket.end()));
+    }
+    NegateSignBit(nums);
+  }
 };
+
+void PrintVectorElements(vector<int> &nums) {
+  std::cout << "\n==== Print Vector Elements ====" << std::endl;
+  for (const int &num : nums)
+    std::cout << num << ' ';
+  std::cout << std::endl;
+}
+
+int main() {
+  Solution solution;
+  vector<int> nums = 
+      {5, 1, -3, -1285, 324, 34, -626, 3256, -514, -346, 434, -3513};
+  PrintVectorElements(nums);
+  solution.RadixSort(nums);
+  PrintVectorElements(nums);
+  return 0;
+}
