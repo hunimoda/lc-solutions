dd5197cebd15a3dea6df512e436603d06284bca2
Dae Hoon Kim <hunimoda@gmail.com>
2022-06-01 15:10:21 +0900

    Easy: problem 575. distribute candies - unordered_set

--- Easy/0500-0599/0575_Distribute_Candies/main.cpp
+++ Easy/0500-0599/0575_Distribute_Candies/main.cpp
@@ -1,12 +1,12 @@
 #include <vector>
-#include <set>
+#include <unordered_set>
 
 using std::vector;
 
 class Solution {
  public:
   int DistributeCandies(vector<int> &candy_type) {
-    std::set<int> candy_set(candy_type.begin(), candy_type.end());
+    std::unordered_set<int> candy_set(candy_type.begin(), candy_type.end());
     return std::min(candy_type.size() / 2, candy_set.size());
   }
 };
