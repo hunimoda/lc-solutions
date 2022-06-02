5fc564303fe73692d03549cd8c7e649c1e6c059d
Dae Hoon Kim <hunimoda@gmail.com>
2022-06-02 18:21:00 +0900

    Easy: problem 628. maximum product of three numbers - stack O(N)

--- Easy/0600-0699/0628_Maximum_Product_of_Three_Numbers/main.cc
+++ Easy/0600-0699/0628_Maximum_Product_of_Three_Numbers/main.cc
@@ -1,7 +1,12 @@
+#include <iostream>
 #include <vector>
 #include <algorithm>
+#include <stack>
 
 using std::vector;
+using std::stack;
+
+void PrintVector(const vector<int> &);
 
 class Solution {
  public:
@@ -11,14 +16,70 @@ class Solution {
     vector<int> pos, neg;
     for (const int num : nums)
       num >= 0 ? pos.push_back(num) : neg.push_back(num);
-    std::sort(pos.begin(), pos.end(), std::greater<>());
-    std::sort(neg.begin(), neg.end(), std::greater<>());
+    
+    if (pos.size() == 0)
+      return ProductOfExtremeValues(neg);
+    if (pos.size() < 3) {
+      return GetExtremeValues(pos, 1)[0] *
+             ProductOfExtremeValues(neg, 2, false);
+    }
+    if (neg.size() < 2)
+      return ProductOfExtremeValues(pos);
 
-    if (pos.size() == 0) return neg[0] * neg[1] * neg[2];
-    if (pos.size() < 3)
-      return pos[0] * neg[neg.size() - 1] * neg[neg.size() - 2];
-    if (neg.size() < 2) return pos[0] * pos[1] * pos[2];
-    return pos[0] * std::max(pos[1] * pos[2],
-                             neg[neg.size() - 1] * neg[neg.size() - 2]);
+    vector<int> largest_pos = GetExtremeValues(pos);
+    const int largest_pos_product = CalculateProduct(largest_pos);
+    return std::max(largest_pos_product,
+                    largest_pos[0] * ProductOfExtremeValues(neg, 2, false));
+  }
+
+ private:
+  int ProductOfExtremeValues(const vector<int> &nums, const int count = 3,
+                             bool is_largest = true) {
+    return CalculateProduct(GetExtremeValues(nums, count, is_largest));
+  }
+  
+  int CalculateProduct(const vector<int> &nums) {
+    int product = 1;
+    for (const int num : nums)
+      product *= num;
+    return product;
+  }
+
+  int CalculateProduct(vector<int> &&nums) {
+    return CalculateProduct(nums);
+  }
+
+  vector<int> GetExtremeValues(const vector<int> &nums, const int count = 3,
+                               bool is_largest = true) {
+    stack<int> s, temp;
+    for (const int num : nums) {
+      while (!s.empty() && (num > s.top() == is_largest))
+        temp.push(s.top()), s.pop();
+      s.push(num);
+      while (!temp.empty())
+        s.push(temp.top()), temp.pop();
+      if (s.size() > count) s.pop();
+    }
+
+    vector<int> extreme;
+    while (!s.empty())
+      extreme.push_back(s.top()), s.pop();
+    std::reverse(extreme.begin(), extreme.end());
+    return extreme;
   }
 };
+
+void PrintVector(const vector<int> &nums) {
+  std::cout << "---- Print Vector----------------------------" << std::endl;
+  std::cout << "  ";
+  for (const int num : nums)
+    std::cout << num << " ";
+  std::cout << std::endl;
+}
+
+int main() {
+  Solution solution;
+  vector<int> nums{1, 6, 4, 2, -8, 9, 2, -4};
+  solution.MaximumProduct(nums);
+  return 0;
+}
