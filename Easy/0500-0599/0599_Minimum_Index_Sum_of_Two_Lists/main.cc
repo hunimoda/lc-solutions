2eb71890f7d0cac92ecdc87c1dcf340584fa37a3
Dae Hoon Kim <hunimoda@gmail.com>
2022-06-02 13:04:01 +0900

    Easy: problem 599. minimum index sum of two lists - clearing vector on fly

--- Easy/0500-0599/0599_Minimum_Index_Sum_of_Two_Lists/main.cc
+++ Easy/0500-0599/0599_Minimum_Index_Sum_of_Two_Lists/main.cc
@@ -10,24 +10,25 @@ using std::unordered_map;
 class Solution {
  public:
   vector<string> FindRestaurant(vector<string> &list1, vector<string> &list2) {
-    unordered_map<string, int> indexes, common;
+    unordered_map<string, int> indexes;
     const size_t list1_size = list1.size();
     const size_t list2_size = list2.size();
 
     for (size_t i = 0; i < list1_size; i++)
       indexes[list1[i]] = i;
 
+    vector<string> common_list;
     int min_index_sum = std::numeric_limits<int>::max();
+
     for (size_t i = 0; i < list2_size; i++) {
       if (!indexes.count(list2[i])) continue;
-      common[list2[i]] = indexes[list2[i]] + i;
-      min_index_sum = std::min(min_index_sum, common[list2[i]]);
-    }
-
-    vector<string> common_list;
-    for (const auto &[restaurant, index_sum] : common) {
-      if (index_sum == min_index_sum)
-        common_list.push_back(restaurant);
+      const int index_sum = indexes[list2[i]] + i;
+      if (index_sum > min_index_sum) continue;
+      if (index_sum < min_index_sum) {
+        common_list.clear();
+        min_index_sum = index_sum;
+      }
+      common_list.push_back(list2[i]);
     }
     return common_list;
   }
