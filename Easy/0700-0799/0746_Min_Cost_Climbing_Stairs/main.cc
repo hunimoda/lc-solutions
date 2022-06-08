904e6bde8d24b0b26dcc6f39e5fe15a5d8e0ea22
Dae Hoon Kim <hunimoda@gmail.com>
2022-06-08 14:38:20 +0900

    Easy: problem 746. min cost climbing stairs - linear tc, constant sc

--- Easy/0700-0799/0746_Min_Cost_Climbing_Stairs/main.cc
+++ Easy/0700-0799/0746_Min_Cost_Climbing_Stairs/main.cc
@@ -4,18 +4,9 @@ using std::vector;
 
 class Solution {
  public:
-  int MinCostClimbingStairs(const vector<int> &cost) {
-    const int stair_length = cost.size();
-    vector<int> min_cost(stair_length);
-
-    // Initial condition
-    min_cost[stair_length - 1] = cost[stair_length - 1];
-    min_cost[stair_length - 2] = cost[stair_length - 2];
-
-    // Calculate min cost from the back towards front
-    for (int i = stair_length - 3; i >= 0; i--)
-      min_cost[i] = cost[i] + std::min(min_cost[i + 1], min_cost[i + 2]);
-
-    return std::min(min_cost[0], min_cost[1]);
+  int MinCostClimbingStairs(vector<int> &cost) {
+    for (int i = cost.size() - 3; i >= 0; i--)
+      cost[i] += std::min(cost[i + 1], cost[i + 2]);
+    return std::min(cost[0], cost[1]);
   }
 };
