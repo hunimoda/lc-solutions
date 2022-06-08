696a2985b68ece4adc7045a3e7716584e7bc9de4
Dae Hoon Kim <hunimoda@gmail.com>
2022-06-08 13:58:02 +0900

    Easy: problem 744. find smallest letter greater than target - binary search

--- Easy/0700-0799/0744_Find_Smallest_Letter_Greater_Than_Target/main.cc
+++ Easy/0700-0799/0744_Find_Smallest_Letter_Greater_Than_Target/main.cc
@@ -7,8 +7,17 @@ class Solution {
   char NextGreatestLetter(const vector<char> &letters, char target) {
     if (target >= letters.back() || target < letters.front())
       return letters.front();
-    int i = 0;
-    while (target >= letters[i]) i++;
-    return letters[i];
+
+    int start = 1;
+    int end = letters.size() - 1;
+    while (start < end) {
+      const int mid = start + (end - start) / 2;
+      if (target >= letters[mid]) {
+        start = mid + 1;
+      } else {
+        end = mid;
+      }
+    }
+    return letters[start];
   }
 };
