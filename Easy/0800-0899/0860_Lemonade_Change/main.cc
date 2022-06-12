99ee7707d065f200dd056f3cb69e5142c3a0aa4f
Dae Hoon Kim <hunimoda@gmail.com>
2022-06-12 13:25:47 +0900

    Easy: problem 860. lemonade change - lee's version of code

--- Easy/0800-0899/0860_Lemonade_Change/main.cc
+++ Easy/0800-0899/0860_Lemonade_Change/main.cc
@@ -7,18 +7,11 @@ class Solution {
   bool LemonadeChange(const vector<int> &bills) {
     int fives = 0, tens = 0;
     for (auto bill : bills) {
-      if (bill == 5) {
-        fives++;
-      } else if (bill == 10) {
-        if (!fives) return false;
-        tens++, fives--;
-      } else if (tens && fives) {
-        tens--, fives--;
-      } else if (fives >= 3) {
-        fives -= 3;
-      } else {
-        return false;
-      }
+      if (bill == 5) fives++;
+      else if (bill == 10) tens++, fives--;
+      else if (tens) tens--, fives--;
+      else fives -= 3;
+      if (fives < 0) return false;
     }
     return true;
   }
