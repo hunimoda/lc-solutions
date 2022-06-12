50d39d27529582668ea2e18583e5ac7f92dc6951
Dae Hoon Kim <hunimoda@gmail.com>
2022-06-12 12:12:36 +0900

    Easy: problem 852. peak index in a mountain array - O(logN)

--- Easy/0800-0899/0852_Peak_Index_in_a_Mountain_Array/main.cc
+++ Easy/0800-0899/0852_Peak_Index_in_a_Mountain_Array/main.cc
@@ -5,8 +5,11 @@ using std::vector;
 class Solution {
  public:
   int PeakIndexInMountainArray(const vector<int> &arr) {
-    int peak = 0;
-    while (arr[peak + 1] > arr[peak]) peak++;
-    return peak;
+    int begin = 0, end = arr.size() - 1;
+    while (begin < end) {
+      int mid = (begin + end) / 2;
+      arr[mid] > arr[mid + 1] ? end = mid : begin = mid + 1;
+    }
+    return begin;
   }
 };
