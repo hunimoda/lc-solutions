ec8edbdd5ee3e782e71c6f585b3752855eb355dd
Hunimoda <hunimoda@gmail.com>
2022-08-14 23:08:09 +0900

    Easy: problem 1051. height checker

---
+++ Easy/1000-1099/1051_Height_Checker/main.cc
@@ -0,0 +1,19 @@
+#include <vector>
+#include <algorithm>
+
+using std::vector;
+using std::sort;
+
+class Solution {
+ public:
+  int HeightChecker(vector<int> &heights) {
+    int count = 0;
+    vector<int> expected = heights;
+
+    sort(expected.begin(), expected.end());
+    for (int i = 0; i < heights.size(); i++)
+      if (heights[i] != expected[i]) count++;
+
+    return count;
+  }
+};
